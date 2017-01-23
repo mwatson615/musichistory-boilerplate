1ec80214a028a6f4e90028ba3154b104fcbc8fb3
Michael Watson <m.watson.tn@gmail.com>
2017-01-22 19:24:06 -0600

    mostly complete specs, working add func

---
+++ songs.js
@@ -0,0 +1,29 @@
+console.log("hi")
+var songs = [];
+
+// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+
+
+document.getElementById("addButton").addEventListener("click", function() {
+	addSong();
+
+	// console.log("hello")
+})
+
+// console.log(songs)
+function addSong() {
+	var addedSong = document.getElementById("songInput").value;
+	songs.push(addedSong)
+	addedSong = '';
+	// console.log(songs)
+	var songDisplay = "";
+	for (i = 0; i < songs.length; i++) {
+		songDisplay += `<h4>${songs[i]}</h4>`
+	}
+	document.getElementById("songAdd").innerHTML = songDisplay;
+}
