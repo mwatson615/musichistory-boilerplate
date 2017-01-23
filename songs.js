ad2b3485b1b223437ed420594dfa2935a1c7b839
Michael Watson <m.watson.tn@gmail.com>
2017-01-22 19:42:15 -0600

    unclear on original setup, understand .replace func

--- songs.js
+++ songs.js
@@ -1,15 +1,22 @@
 console.log("hi")
 var songs = [];
 
-// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
+function fixSongs() {
+	for (i = 0; i < songs.length; i++) {
+	songs[i].replace(/>/, "-")
+	}
+	console.log(songs)
+}
 
 
 document.getElementById("addButton").addEventListener("click", function() {
+	fixSongs();
 	addSong();
 
 	// console.log("hello")
