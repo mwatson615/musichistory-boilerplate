6ed209d86fe3483060c833a9c1722f5c21d88bf0
Michael Watson <m.watson.tn@gmail.com>
2017-01-22 23:20:14 -0600

    set up and first evt listner

--- songs.js
+++ songs.js
@@ -1,36 +1,61 @@
 console.log("hi")
 var songs = [];
-
-// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-
-function fixSongs() {
-	for (i = 0; i < songs.length; i++) {
-	songs[i].replace(/>/, "-")
-	}
-	console.log(songs)
-}
-
+var artists = [];
+var albums = [];
+//music history 3
 
 document.getElementById("addButton").addEventListener("click", function() {
-	fixSongs();
-	addSong();
+	// fixSongs();
+	addInfo();
 
 	// console.log("hello")
 })
 
+var addView = document.getElementById('addView')
+var listView = document.getElementById('listView')
+var homeView = document.getElementById('homeView')
+
+var homePage = document.getElementById('home')
+var listPage = document.getElementById('listMusic')
+var addPage = document.getElementById('addMusic')
+
+addView.addEventListener('click', function() {
+	listPage.setAttribute("class", "hidden")
+	addPage.classList.remove("hidden")
+	homePage.setAttribute("class", "hidden")
+	// console.log("werd")
+})
+
+
+
+//music history 2
+songs.push("Legs", "The Logical Song", "Another Brick in the Wall", "Welcome to the Jungle", "Ironic");
+artists.push("ZZ Top", "Supertramp", "Pink Floyd", "Guns & Roses", "Alanis Morissette");
+albums.push("Eliminator", "Breakfast in America", "The Wall", "Appetite for Destruction", "Jagged Little Pill");
+
+// function fixSongs() {
+// 	for (i = 0; i < songs.length; i++) {
+// 	songs[i].replace(/>/, "-")
+// 	}
+// 	console.log(songs)
+// }
+
+
+
+
 // console.log(songs)
-function addSong() {
+function addInfo() {
 	var addedSong = document.getElementById("songInput").value;
-	songs.push(addedSong)
-	addedSong = '';
+	var addedArtist = document.getElementById("artistInput").value;
+	var addedAlbum = document.getElementById("albumInput").value;
+	songs.push(addedSong);
+	artists.push(addedArtist);
+	albums.push(addedAlbum);
+	// addedSong = '';
 	// console.log(songs)
 	var songDisplay = "";
 	for (i = 0; i < songs.length; i++) {
-		songDisplay += `<h4>${songs[i]}</h4>`
+		songDisplay += `<h4>${songs[i]} by ${artists[i]} from the album ${albums[i]}</h4>`
 	}
-	document.getElementById("songAdd").innerHTML = songDisplay;
+	document.getElementById("songList").innerHTML = songDisplay;
 }
